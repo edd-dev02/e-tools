dc49daa3c1d6f2c04133e46f2dde7eef7f8e6d62
Eduardo Sandoval <edsandoval.dev@gmail.com>
2026-06-21 13:49:57 -0600

    Creacion estructura principal del módulo salary-distributor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidemenuComponent } from './shared/sidemenu/sidemenu.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SidemenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/salary-distributor/pages/salary-distributor-page/salary-distributor-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-salary-distributor-page',
+  imports: [],
+  templateUrl: './salary-distributor-page.component.html',
+  styleUrl: './salary-distributor-page.component.css',
+})
+export class SalaryDistributorPage {
+
+}
