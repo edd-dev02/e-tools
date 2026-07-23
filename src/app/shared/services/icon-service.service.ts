c48935d8ede2d71dc216610fee1bcb570924fc55
Eduardo Sandoval <edsandoval.dev@gmail.com>
2026-07-22 22:01:28 -0600

    Creación de servicio para el manejo de iconos SVG en toda la aplicacion | Se adjuntan iconos de la sección 1 de salary-distributor

---
+++ src/app/shared/services/icon-service.service.ts
@@ -0,0 +1,42 @@
+import { Injectable, inject } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IconService {
+
+  private iconRegistry = inject(MatIconRegistry);
+  private sanitizer = inject(DomSanitizer);
+
+  constructor() {
+
+    this.registerIcons();
+
+  }
+
+  public registerIcons(): void {
+
+    this.addIcons('percent', 'icons/percent.svg');
+    this.addIcons('savings', 'icons/savings.svg');
+    this.addIcons('bills', 'icons/bills.svg');
+    this.addIcons('leisure', 'icons/leisure.svg');
+    this.addIcons('efund', 'icons/efund.svg');
+    this.addIcons('check', 'icons/check.svg');
+    this.addIcons('warning', 'icons/warning.svg');
+    this.addIcons('error', 'icons/error.svg');
+    this.addIcons('restart', 'icons/restart.svg');
+
+  }
+
+  public addIcons(iconName: string, iconUrl: string): void {
+    this.iconRegistry.addSvgIcon(
+      iconName,
+      this.sanitizer.bypassSecurityTrustResourceUrl(
+        iconUrl
+      )
+    );
+  }
+
+}
