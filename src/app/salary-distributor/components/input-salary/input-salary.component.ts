d2b3f9fb82a7c78e83a1ef06eccbfb90a6aa7741
Eduardo Sandoval <edsandoval.dev@gmail.com>
2026-07-25 11:01:59 -0600

    feature: creacion de componente principal para ingresar el salario total

---
+++ src/app/salary-distributor/components/input-salary/input-salary.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-input-salary',
+  imports: [],
+  templateUrl: './input-salary.component.html',
+  styleUrl: './input-salary.component.css',
+})
+export class InputSalary {
+
+}
