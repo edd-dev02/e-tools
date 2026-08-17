0a7c5907f464364c017feee2bd26c93d79ab0380
Eduardo Sandoval <edsandoval.dev@gmail.com>
2026-08-16 21:40:03 -0600

    post deployment fix: Se elimina animación de scroll en componente input-salary

--- src/app/salary-distributor/components/input-salary/input-salary.component.ts
+++ src/app/salary-distributor/components/input-salary/input-salary.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { AfterViewInit, Component, ElementRef, inject, input, output } from '@angular/core';
+import { Component, inject, input, output } from '@angular/core';
 import { NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
@@ -19,21 +19,11 @@ import { DistributeCalculus } from '@interfaces/distribute-calculus.interface';
   templateUrl: './input-salary.component.html',
   styleUrl: './input-salary.component.css',
 })
-export class InputSalaryComponent implements AfterViewInit {
+export class InputSalaryComponent {
 
   private fb = inject(NonNullableFormBuilder);
   private is = inject(IconService);
   private salaryDistributorService = inject(SalaryDistributorService);
-  private elementRef = inject(ElementRef);
-
-  ngAfterViewInit(): void {
-    setTimeout(() => {
-      this.elementRef.nativeElement.scrollIntoView({
-        behavior: 'smooth',
-        block: 'start'
-      });
-    });
-  }
 
   public percentagesValues = input<PercentagesForm>()
 
